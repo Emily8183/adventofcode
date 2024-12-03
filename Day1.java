35c4eca9b20da94584361781810f77886699188c
Emily <emily.zy.lin@gmail.com>
2024-12-02 21:58:39 -0600

    set up day 1 exercise

---
+++ Day1.java
@@ -0,0 +1,3 @@
+//goal: to find the smallest num from both of the left and right list, cal the difference, and add up
+//solution: bruce force: find the smallest num with Java method
+//to read the file: BufferedReader and FileReader
