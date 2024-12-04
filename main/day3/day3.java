6f082678ac6c3aa3e2c3f01b4a1aa46a5d1884f5
Emily <emily.zy.lin@gmail.com>
2024-12-03 21:22:18 -0600

    complete day3 part1 with Regex

--- main/day3/day3.java
+++ main/day3/day3.java
@@ -1,23 +1,58 @@
 /**
  * part1:
  * goal: find the exact format of mul(num,num);
- * solution: stack
+ * solution: Regex, matcher.group
  */
 
 
 
 package main.day3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class day3 {
     public int calMultiply(String content) {
-        Stack<String> stack = new Stack<>();
-        char[] arr 
 
-        for (char c : content.toCharArray()) {
+        int multiply = 0;
 
+        try (BufferedReader readData = new BufferedReader(new FileReader(content))) {
+
+        String line;
+
+        String reqPattern = "mul\\((\\d+),(\\d+)\\)";
+
+        StringBuilder fullText = new StringBuilder();
+        
+        Pattern pattern = Pattern.compile(reqPattern);
+
+        while ((line = readData.readLine()) != null) {
+
+            fullText.append(line).append(" "); 
         }
 
+        Matcher matcher = pattern.matcher(fullText.toString());
+
+        while (matcher.find()) { //matches() checks if the entire string matches the patter
+            String firstNum = matcher.group(1);
+            String secondNum = matcher.group(2);
+
+            System.out.println("Matched: mul(" + firstNum + "," + secondNum + ")");
+
+            multiply += (Integer.parseInt(firstNum) * Integer.parseInt(secondNum));
+        }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return multiply;
 
     }
-    
+
+
 }
+    
+
--- main/solution.java
+++ main/solution.java
@@ -1,23 +1,24 @@
 package main;
 import main.day1.day1;
 import main.day2.day2;
+import main.day3.day3;
 
 public class solution {
     public static void main(String[] args) {
 
         // day1 day1 = new day1();
         // String content = "main/day1/day1data.txt";
-
         // day1.day1solution(content);
 
-        day2 day2 = new day2();
-        // String content = "main/day2/day2data.txt";
-        
-        String content = "main/day2/day2extratest.txt";
+        // day2 day2 = new day2();        
+        // String content = "main/day2/day2extratest.txt";
+        // int countResult = day2.countReport(content);
+        // System.out.println(countResult);
 
-        int countResult = day2.countReport(content);
-
-        System.out.println(countResult);
+        day3 day3 = new day3();
+        String data = "main/day3/day3data.txt";
+        int result = day3.calMultiply(data);
+        System.out.println(result);
         
     }
     
